8ea1ed75a14759e2f7a67d3228fb140f0d9521bc
xuanc <congnguyenxuan@gmail.com>
2019-12-02 14:21:22 -0500

    jiu

--- app/src/main/java/com/example/cs391_ble/BLEConnect.kt
+++ app/src/main/java/com/example/cs391_ble/BLEConnect.kt
@@ -42,13 +42,30 @@ const val ACTION_GATT_SERVICES_DISCOVERED =
 /**
  * Coordinate-based system!
  */
-private val BEACON1_COORD = Pair(1,0) //beacon 1 on right of diagram
-private val BEACON2_COORD = Pair(0,2) // beacon 2 in middle
-private val BEACON3_COORD = Pair(-1,0) // beacon 3 on left
+private val BEACON1_COORD = Pair(1.0,0.0) //beacon 1 on right of diagram
+private val BEACON2_COORD = Pair(0.0,2.0) // beacon 2 in middle
+private val BEACON3_COORD = Pair(-1.0,0.0) // beacon 3 on left
 
-var rssi1:Int = 0
+var rssi1:Int= 0
 var rssi2:Int = 0
 var rssi3:Int = 0
+var distance: Double = 0.0
+var txPower : Double = -23.0
+val pointA1: Double = 1.0
+val pointA2: Double= 0.0
+val pointB1: Double = 0.0
+val pointB2: Double = 2.0
+val pointC1: Double = -1.0
+val pointC2: Double = 0.0
+var distanceA: Double = 0.0
+var distanceB : Double = 0.0
+var distanceC : Double = 0.0
+var w: Double = 0.0
+var z: Double = 0.0
+var x: Double = 0.0
+var y: Double = 0.0
+var y2: Double = 0.0
+
 
 private const val SYS_DELAY = 0.001 // Reading each Device's rssi creates lag...  Subtract from signal received
 private const val SIGNAL_S = 0.3    //Speed(m) of signal per ns....
@@ -173,6 +190,12 @@ class BLEConnect: AppCompatActivity()  {
                 Beacon2RSSI.text=Integer.toString(rssi2) + " dBm"
                 Beacon3RSSI.text=Integer.toString(rssi3) + " dBm"
                 //angleCalc(bluetoothGatt)
+                distanceA = calculateBeaconDistance(rssi1)
+                distanceB = calculateBeaconDistance(rssi2)
+                distanceC = calculateBeaconDistance(rssi3)
+                trilateration()
+                Location.text= x.toString() + y.toString()
+
             }
         }
         /**
@@ -213,8 +236,55 @@ class BLEConnect: AppCompatActivity()  {
 
 
 }
+/*The values 0.89976, 7.7095 and 0.111 are the three constants
+calculated when solving for a best fit curve*/
+fun calculateBeaconDistance(rssi: Int): Double {
+    var rssiD = rssi.toDouble()
+    var ratio : Double
+    var accuracy : Double
+
+    txPower = -23.0; // Manufacture set this power in the device
+    if (rssiD == 0.0){
+
+       return  -1.0; // if we cannot determine accuracy, return -1.
+
+    }
+
+    ratio = rssi*1.0 / txPower;
+    if (ratio < 1.0){
+        return  Math.pow(ratio,10.0);
+
+    }
+    else{
+        accuracy = (0.89976)*Math.pow(ratio,7.7095) + 0.111;
+        return accuracy;
+    }
+}
+
+/**
+ * It needs distanceA is distance from beacon 1, distanceB is distance from beacon 2, distanceC is distance from beacon 3
+ * , pointA1, pointA2 (location of beacon 1) (1.0,0.0)
+ * , pointB1, pointB2  (location of beacon 2) (0.0,2.0)
+ * , pointC1, pointC2 (location of beacon 3)  (-1.0,0.0)
+ * x, y are the location of the device
+ */
+
+fun trilateration() {
 
 
+    w = distanceA * distanceA - distanceB * distanceB - pointA1 * pointA1 - pointA2* pointA2 + pointB1 * pointB1 + pointB2 * pointB2;
+
+    z = distanceB * distanceB - distanceC * distanceC - pointB1* pointB1 - pointB2 * pointB2 + pointC1 * pointC1 + pointC2 * pointC2;
+
+    x = (w * ( pointC2 - pointB2) - z * ( pointB2 - pointA2)) / (2 * (( pointB1 - pointA1) * ( pointC1 - pointB2) - ( pointC1 - pointB1) * ( pointB2 - pointA2)));
+
+    y = (w - 2 * x * (pointB1 - pointA1)) / (2 * ( pointB2 - pointA2));
+
+    y2 = (z - 2 * x * ( pointC1 -pointB1)) / (2 * ( pointC1 - pointB2));
+
+    y = (y + y2) / 2;
+
+}
 
 
 
